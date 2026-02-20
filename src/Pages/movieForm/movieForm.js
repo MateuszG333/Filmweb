426e1acf7a281949e75bad7c2bffe1e4b8420587
Myfyn <mjastrzebski51@gmail.com>
2026-02-20 15:55:09 +0100

    dodawanie filmów

--- src/Pages/movieForm/movieForm.js
+++ src/Pages/movieForm/movieForm.js
@@ -5,34 +5,71 @@ export function MovieForm() {
     title: "",
     description: "",
     director: "",
-    year: "",
   });
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        console.log(formData);
-    };
-    const handleChange = (event) => {
-        const {name, value} = event.target;
+    console.log(formData);
 
-        setFormData(prev => ({
-            ...prev,
-            [name]: value
-        }));
-    };
+    try {
+      const result = await fetch("http://localhost:3005/api/movies", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (result.status !== 201) {
+        console.error("wrong status");
+        return;
+      }
+
+      const newMovie = await result.json();
+      console.log(newMovie);
+      alert("film dodany");
+    } catch (e) {
+      console.error(e.message);
+      alert("Coś poszło nie tak 😢");
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   return (
     <>
       <h1>ADD MOVIE</h1>
 
-      <form>
-        <input type="text" placeholder="Title" name="title" onChange={handleChange}/>
-        <input type="text" placeholder="Description" name="description" onChange={handleChange}/>
-        <input type="text" placeholder="Director"  name="director" onChange={handleChange}/>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="title"
+          type="text"
+          name="title"
+          onChange={handleChange}
+        ></input>
+        <textarea
+          placeholder="description"
+          name="description"
+          onChange={handleChange}
+        ></textarea>
+        <input
+          placeholder="director"
+          type="text"
+          name="director"
+          onChange={handleChange}
+        ></input>
         <button type="submit">DODAJ FILM DO BAZY</button>
       </form>
     </>
   );
 }
+
 export default MovieForm;
