b6df4a58033586890c7ceb4559a795169e4052be
Myfyn <mjastrzebski51@gmail.com>
2026-02-21 11:19:47 +0100

    Update Ostatni

--- src/Pages/movieDetails/movieDetails.js
+++ src/Pages/movieDetails/movieDetails.js
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 
 export default function MovieDetails() {
 
         const { id } = useParams();
+
+        useEffect(() => {}, [id])
+
+            //fetch do backendu po szczegóły filmu o id z paramsów // do api /movies/:id
+            //i naprawić żęby wyświetlało te szczegóły na stronie bo nie działa, a powinno
+
 
     return (
 
